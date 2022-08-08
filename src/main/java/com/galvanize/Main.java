f5402fe4f380af542d601a899c740ddd9b51e6bf
Candy Acosta <caacosta@svb.com>
2022-08-08 15:04:55 -0700

    name/emailFormatter

--- src/main/java/com/galvanize/Main.java
+++ src/main/java/com/galvanize/Main.java
@@ -4,6 +4,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        if(args.length < 1)
+        {
+            System.out.println("Please specify a name and email.");
+        }
+        else if(args.length == 1)
+        {
+            System.out.println("Please specify an email for " + args[0]);
+        }
+        else if(args.length > 1)
+        {
+            System.out.println(String.format("%s", args[0]) + " " + "<" + args[1] + ">");
+        }
+        else
+        {
+            System.out.println(String.format("%s", args[0]) + " " + "<" + args[1] + ">");
+        }
     }
 }
